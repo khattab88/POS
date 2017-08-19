ffb53b825ce4c6a37f32ce3d7024aa2989d9757c
Mahmoud Khattab <m_khatab_88@hotmail.com>
2017-08-19 23:19:46 +0200

    Adding Integration Test Project

--- POS.Data/Migrations/Configuration.cs
+++ POS.Data/Migrations/Configuration.cs
@@ -7,7 +7,7 @@ namespace POS.Data.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<POS.Data.POSEntities>
+    public sealed class Configuration : DbMigrationsConfiguration<POS.Data.POSEntities>
     {
         public Configuration()
         {
---
+++ POS.IntegrationTests/GlobalSetup.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.IntegrationTests
+{
+    [SetUpFixture]
+    public class GlobalSetup
+    {
+        [SetUp]
+        public void SetUp() 
+        {
+            // bring test database to latest version
+            var configuration = new POS.Data.Migrations.Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
+
+
+        }
+    }
+}
